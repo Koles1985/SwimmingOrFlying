857151e6f19e061261b7362adb017721ed4fd30d
User <User@DESKTOP-193EUN2>
2022-04-01 23:00:01 +0300

    Solving task

---
+++ src/com/koles/vehicles/JamesBondCar.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.koles.vehicles;
+
+import com.koles.skills.Swimmable;
+import com.koles.skills.Flyable;
+
+/**
+ *
+ * @author User
+ */
+public class JamesBondCar implements Swimmable, Flyable {
+    
+}
---
+++ src/com/koles/vehicles/Zeppelin.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.koles.vehicles;
+
+import com.koles.skills.Flyable;
+
+/**
+ *
+ * @author User
+ */
+public class Zeppelin implements Flyable {
+    
+}
